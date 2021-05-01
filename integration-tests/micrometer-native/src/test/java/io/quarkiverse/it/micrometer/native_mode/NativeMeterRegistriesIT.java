8177783d09c6406ab3f6eea3429a164858e60f80
Cem Nura <cem.nura@gmail.com>
2021-05-01 03:06:47 +0300

    Added New Relic to micrometer native IT tests
    Signed-off-by: Cem Nura <cem.nura@gmail.com>

--- integration-tests/micrometer-native/src/test/java/io/quarkiverse/it/micrometer/native_mode/NativeMeterRegistriesIT.java
+++ integration-tests/micrometer-native/src/test/java/io/quarkiverse/it/micrometer/native_mode/NativeMeterRegistriesIT.java
@@ -27,6 +27,7 @@ class NativeMeterRegistriesIT extends NativeMeterRegistriesTest {
         List<Object> registries = response.jsonPath().getList("$");
         MatcherAssert.assertThat(registries, Matchers.containsInAnyOrder(
                 "io.micrometer.datadog.DatadogMeterRegistry",
+                "io.micrometer.newrelic.NewRelicMeterRegistry",
                 "io.micrometer.stackdriver.StackdriverMeterRegistry"));
 
         MatcherAssert.assertThat(registries, Matchers.not(Matchers.containsInAnyOrder(
